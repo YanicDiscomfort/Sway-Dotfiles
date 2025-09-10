706043a34c9000848ec58630189f90a1f5879223
Yanic <yanic.dev@gmail.com>
2025-09-10 15:36:46 +0200

    add install script for basic packages

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+base=( dracut base-devel git neovim yazi bc zsh mesa amd-ucode unzip zip 7zip unrar-free btop )
+desktop=( wayland xorg-xwayland wlroots sway swaylock swaybg uwsm ly swaync waybar rofi-wayland rofi-emoji pipewire wireplumber pipewire-pulse pipewire-alsa pipewire-jack pipewire-audio xdg-desktop-portal-wlr )
+applications=( thunar file-roller thunar-archive-plugin thunar-media-plugin ffmpeg ffmpegthumbnailer tumbler kitty qt5 qt6 qt6ct gtk3 gtk4 nwg-look pavucontrol mpv amberol vivaldi )
+flatpak=( xdg-desktop-portal-gtk flatpak )
+yay=( go )
+shenaniganz=( sl uwufetch viu )
