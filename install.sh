4b77bcea8302ba5988810a3f8d787f4dcba2c07d
yanic <yanic.dev@gmail.com>
2025-09-11 14:35:24 +0200

    add cursor install and thunar config

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 base=( dracut base-devel git neovim yazi bc zsh mesa amd-ucode unzip zip 7zip unrar-free btop bluez )
-desktop=( wayland xorg-xwayland sway swaylock swaybg uwsm ly swaync waybar rofi-wayland rofi-emoji pipewire wireplumber pipewire-pulse pipewire-alsa pipewire-jack pipewire-audio xdg-desktop-portal-wlr papirus-icon-theme udiskie)
+desktop=( wayland xorg-xwayland sway swaylock swaybg uwsm ly swaync waybar rofi-wayland rofi-emoji pipewire wireplumber pipewire-pulse pipewire-alsa pipewire-jack pipewire-audio xdg-desktop-portal-wlr papirus-icon-theme udiskie flameshot brightnessctl blueberry )
 applications=( thunar file-roller thunar-archive-plugin thunar-media-tags-plugin ffmpeg imagemagick ffmpegthumbnailer tumbler kitty qt5 qt6 qt6ct gtk3 gtk4 nwg-look pavucontrol mpv amberol vivaldi )
 fonts=( nerd-fonts adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts adobe-source-han-sans-hk-fonts adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-sans-otc-fonts adobe-source-han-sans-tw-fonts adobe-source-han-serif-cn-fonts adobe-source-han-serif-hk-fonts adobe-source-han-serif-jp-fonts adobe-source-han-serif-kr-fonts adobe-source-han-serif-otc-fonts adobe-source-han-serif-tw-fonts adobe-source-sans-fonts adobe-source-serif-fonts cantarell-fonts gnu-free-fonts gsfonts noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra tex-gyre-fonts texlive-fontsrecommended ttf-dejavu ttf-droid ttf-hack ttf-inconsolata ttf-liberation ttf-roboto ttf-ubuntu-font-family xorg-fonts-misc )
 flatpak=( xdg-desktop-portal-gtk flatpak )
@@ -20,6 +20,10 @@ sudo pacman -S ${shenaniganz[@]}
 
 sudo chsh -s /bin/zsh $USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp zshrc ~/.zshrc
+
+sudo cp cursor/Bibata-Modern-Classic /usr/share/icons -r
+sudo cp cursor/index.theme /usr/share/icons/default/index.theme
 
 cd ~
 git clone https://aur.archlinux.org/yay.git
@@ -29,6 +33,5 @@ cd ~
 rm -rf yay
 
 sudo systemctl enable ly bluetooth
-
 
 reboot
